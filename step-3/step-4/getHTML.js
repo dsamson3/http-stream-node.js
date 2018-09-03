48a19a732b3b347b0a639af360a7b51e9de8b98b
Dave <dave.samson91@gmail.com>
2018-09-03 19:25:45 +0000

    Callback function placed as parameter

---
+++ step-3/step-4/getHTML.js
@@ -0,0 +1,21 @@
+function getHTML (options, callBack){
+    var chunks = '';
+    var https = require('https')
+  https.get(options, function(reponse){
+      reponse.setEncoding('UTF-8');
+      reponse.on("data", function(data){
+          chunks += data;
+      });
+      reponse.on("end", function(){
+          return callBack(chunks);
+      });
+  });
+}
+function printHTML (html){
+    console.log(html);
+}
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+}
+console.log(getHTML(requestOptions, printHTML));
